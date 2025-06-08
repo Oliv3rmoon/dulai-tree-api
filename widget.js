7b0ed6cabac02382a3a8a77950ba504a933560d9
Oliv3rmoon <165517657+Oliv3rmoon@users.noreply.github.com>
2025-06-08 02:07:42 -0700

    Create widget.js

---
+++ widget.js
@@ -0,0 +1,7 @@
+<!-- how you’ll load it in Squarespace -->
+<script async
+        src="https://dulai-tree-api.onrender.com/static/widget.js"
+        data-api="https://dulai-tree-api.onrender.com/chat"
+        data-title="Dulai Tree Experts"
+        data-accent="#1b9b3a"
+></script>
